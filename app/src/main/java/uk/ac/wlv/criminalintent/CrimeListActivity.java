69afb7dc03ab02355e5e24c78cd74733d347479f
KiwiFarah <warnakulasuriyafarah@gmail.com>
2023-09-24 17:21:57 +0530

    Workshop 6 Task 1 complete

---
+++ app/src/main/java/uk/ac/wlv/criminalintent/CrimeListActivity.java
@@ -0,0 +1,21 @@
+package uk.ac.wlv.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+public class CrimeListActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_activity_crime);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if(fragment == null){
+            fragment = new CrimeListFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
